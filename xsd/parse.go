bab430db49e3c7330d77bf3bee3895528e47c678
Tom Gehrke <phyrog@gmail.com>
2024-09-19 22:33:58 +0200

    when choice as maxOccurs=unbounded, apply to all children

--- xsd/parse.go
+++ xsd/parse.go
@@ -78,13 +78,13 @@ func Imports(data []byte) ([]Ref, error) {
 // Normalize reads XML schema documents and returns xml trees
 // for each schema with the following properties:
 //
-// * various XSD shorthand, such as omitting <complexContent>,
-//   are expanded into their canonical forms.
-// * all links are dereferenced by merging the linked element.
-// * all types have names. For anonymous types, unique (per
-//   namespace) names of the form "_anon1", "_anon2", etc are
-//   generated, and the attribute "_isAnonymous" is set to
-//   "true".
+//   - various XSD shorthand, such as omitting <complexContent>,
+//     are expanded into their canonical forms.
+//   - all links are dereferenced by merging the linked element.
+//   - all types have names. For anonymous types, unique (per
+//     namespace) names of the form "_anon1", "_anon2", etc are
+//     generated, and the attribute "_isAnonymous" is set to
+//     "true".
 //
 // Because one document may contain more than one schema, the
 // number of trees returned by Normalize may not equal the
@@ -187,19 +187,25 @@ func anonTypeName(n int, ns string) xml.Name {
 	return xml.Name{ns, fmt.Sprintf("_anon%d", n)}
 }
 
-/* Convert
+/*
+	Convert
+
 <element name="foo">
-  <complexType>
-  ...
-  </complexType>
+
+	<complexType>
+	...
+	</complexType>
+
 </element>
 
 to
 
 <element name="foo" type="foo">
-  <complexType name="foo">
-  ...
-  </complexType>
+
+	<complexType name="foo">
+	...
+	</complexType>
+
 </element>
 */
 func copyEltNamesToAnonTypes(root *xmltree.Element) {
@@ -243,6 +249,7 @@ func copyEltNamesToAnonTypes(root *xmltree.Element) {
 func setChoicesOptional(root *xmltree.Element) error {
 
 	for _, el := range root.SearchFunc(isElem(schemaNS, "choice")) {
+		maxOccurs := el.Attr("", "maxOccurs")
 		for i := 0; i < len(el.Children); i++ {
 			t := el.Children[i]
 
@@ -250,10 +257,16 @@ func setChoicesOptional(root *xmltree.Element) error {
 				for j := 0; j < len(t.Children); j++ {
 					t2 := t.Children[j]
 					t2.SetAttr("", "minOccurs", "0")
+					if maxOccurs == "unbounded" {
+						t2.SetAttr("", "maxOccurs", maxOccurs)
+					}
 					t.Children[j] = t2
 				}
 			} else {
 				t.SetAttr("", "minOccurs", "0")
+				if maxOccurs == "unbounded" {
+					t.SetAttr("", "maxOccurs", maxOccurs)
+				}
 			}
 
 			el.Children[i] = t
@@ -266,27 +279,27 @@ func setChoicesOptional(root *xmltree.Element) error {
 /*
 Convert
 
-  <xs:complexType name="foo" base="xs:anyType"/>
-    <xs:sequence>
-      <xs:element name="a">
-        <xs:simpleType base="xs:int">
-          ...
-        </xs:simpleType>
-      </xs:element>
-    </xs:sequence>
-  </xs:complexType>
+	<xs:complexType name="foo" base="xs:anyType"/>
+	  <xs:sequence>
+	    <xs:element name="a">
+	      <xs:simpleType base="xs:int">
+	        ...
+	      </xs:simpleType>
+	    </xs:element>
+	  </xs:sequence>
+	</xs:complexType>
 
 to
 
-  <xs:complexType name="foo" base="xs:anyType"/>
-    <xs:sequence>
-      <xs:element name="a">
-        <xs:simpleType name="_anon1" _isAnonymous="true" base="xs:int">
-          ...
-        </xs:simpleType>
-      </xs:element>
-    </xs:sequence>
-  </xs:complexType>
+	<xs:complexType name="foo" base="xs:anyType"/>
+	  <xs:sequence>
+	    <xs:element name="a">
+	      <xs:simpleType name="_anon1" _isAnonymous="true" base="xs:int">
+	        ...
+	      </xs:simpleType>
+	    </xs:element>
+	  </xs:sequence>
+	</xs:complexType>
 */
 func nameAnonymousTypes(root *xmltree.Element, typeCounter *int) error {
 	var (
@@ -344,20 +357,18 @@ func nameAnonymousTypes(root *xmltree.Element, typeCounter *int) error {
 }
 
 /*
-
 Dereference all ref= links within a document.
 
-  <attribute name="id" type="xsd:ID" />
-  <complexType name="MyType">
-    <attribute ref="tns:id" />
-  </complexType>
+	<attribute name="id" type="xsd:ID" />
+	<complexType name="MyType">
+	  <attribute ref="tns:id" />
+	</complexType>
 
 becomes
 
-  <complexType name="MyType">
-    <attribute name="id" type="xsd:ID" />
-  </complexType>
-
+	<complexType name="MyType">
+	  <attribute name="id" type="xsd:ID" />
+	</complexType>
 */
 func flattenRef(schema []*xmltree.Element) error {
 	var (
@@ -529,11 +540,11 @@ func (s *Schema) addElementTypeAliases(root *xmltree.Element, types map[xml.Name
 // English. My translation may be incorrect; check the reference if you
 // think so. The rules are as follows:
 //
-// 	- When extending a complex type, the derived type *must* be mixed iff
-//    the base type is mixed.
-// 	- When restricting a complex type, the derived type *may* be mixed iff
-//    the base type is mixed.
-// 	- The builtin "xs:anyType" is mixed.
+//   - When extending a complex type, the derived type *must* be mixed iff
+//     the base type is mixed.
+//   - When restricting a complex type, the derived type *may* be mixed iff
+//     the base type is mixed.
+//   - The builtin "xs:anyType" is mixed.
 //
 // This package extends the concept of "Mixed" to apply to complex types
 // with simpleContent. This is done because Mixed is used as an indicator
@@ -591,7 +602,7 @@ func attributeDefaultType(root *xmltree.Element) {
 
 // 3.3.2 XML Representation of Element Declaration Schema Components
 //
-// Elements types default to anyType
+// # Elements types default to anyType
 //
 // https://www.w3.org/TR/xmlschema-1/#Element_Declaration_details
 func elementDefaultType(root *xmltree.Element) {
